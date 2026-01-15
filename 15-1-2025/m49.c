e6c9f96a24cebf6ceb22c50d0e4c3aa1e807aeeb
rmahidhar79-eng <rmahidhar79@gmail.com>
2026-01-15 23:07:06 +0530

    Add files via upload

---
+++ 15-1-2025/m49.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+char str[50];
+int i, j;
+char temp;
+scanf("%s", str);
+j = strlen(str) - 1;
+for (i = 0; i < j; i++, j--) {
+temp = str[i];
+str[i] = str[j];
+str[j] = temp;
+    }
+
+    printf("Reversed string: %s", str);
+
+    return 0;
+}
