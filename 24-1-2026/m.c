cd8a97533696d71be3d486a95cbb994c78f63d8d
rmahidhar79-eng <rmahidhar79@gmail.com>
2026-01-24 23:02:14 +0530

    Add files via upload

---
+++ 24-1-2026/m.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char str[50];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while(str[i] != '\0') {
+        i++;
+    }
+
+    printf("Length = %d", i);
+    return 0;
+}
