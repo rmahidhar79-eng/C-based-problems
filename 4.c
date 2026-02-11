ccd32e6ca472fde6ed12d9fc664d587eecb38768
rmahidhar79-eng <rmahidhar79@gmail.com>
2026-02-11 18:23:32 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U') {
+            count++;
+        }
+    }
+
+    printf("Number of vowels = %d", count);
+
+    return 0;
+}
